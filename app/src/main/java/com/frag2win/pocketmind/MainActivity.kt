25fdd3dfb1d2bbbf846852c7e875604fa3b19b3a
frag2win <shubhmapawar@gmail.com>
2026-07-02 00:48:44 +0530

    feat: initialize MainActivity with ChatScreenRoot and project planning configuration

--- app/src/main/java/com/frag2win/pocketmind/MainActivity.kt
+++ app/src/main/java/com/frag2win/pocketmind/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.frag2win.pocketmind.ui.chat.ChatScreenRoot
 import com.frag2win.pocketmind.ui.theme.PocketMindTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,11 +25,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             PocketMindTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    com.frag2win.pocketmind.ui.chat.ChatScreenRoot(
+                    ChatScreenRoot(
                         modifier = Modifier
                             .padding(innerPadding)
-                            .androidx.compose.foundation.layout.consumeWindowInsets(innerPadding)
-                            .androidx.compose.foundation.layout.imePadding()
+                            .consumeWindowInsets(innerPadding)
+                            .imePadding()
                     )
                 }
             }
