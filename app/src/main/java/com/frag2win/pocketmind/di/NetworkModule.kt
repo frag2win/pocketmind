1419525f4b358e9be18714b85a87e1dc97a67234
frag2win <shubhmapawar@gmail.com>
2026-07-13 22:37:22 +0530

    FEATURE: Update DI for GitHub network clients

--- app/src/main/java/com/frag2win/pocketmind/di/NetworkModule.kt
+++ app/src/main/java/com/frag2win/pocketmind/di/NetworkModule.kt
@@ -1,12 +1,15 @@
 package com.frag2win.pocketmind.di
 
 import com.frag2win.pocketmind.data.local.ModelPreferences
+import com.frag2win.pocketmind.data.remote.GitHubService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
@@ -39,5 +42,15 @@ object NetworkModule {
             .writeTimeout(5, TimeUnit.MINUTES)
             .addInterceptor(authInterceptor)
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubService(): GitHubService {
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(GitHubService::class.java)
     }
 }
